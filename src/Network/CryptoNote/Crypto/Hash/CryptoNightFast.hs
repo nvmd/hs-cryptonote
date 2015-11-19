5cd9a80e1b6bf39f65d7a421350099482fbe1f2a
Sergey Kazenyuk <kazenyuk@gmail.com>
2015-11-20 02:16:23 +0300

    Use cryptonite's cryptography implementations where available

--- src/Network/CryptoNote/Crypto/Hash/CryptoNightFast.hs
+++ src/Network/CryptoNote/Crypto/Hash/CryptoNightFast.hs
@@ -1,8 +1,24 @@
+{-# LANGUAGE CPP #-}
+
 module Network.CryptoNote.Crypto.Hash.CryptoNightFast ( hash ) where
 
+#if defined(USE_FFI_CRYPTO)
 import qualified Network.CryptoNote.Crypto.Hash.FFI as FFI
+#else
+import Crypto.Hash (hashWith)
+import Crypto.Hash.Algorithms (Keccak_256 (..))
+import Data.ByteArray (convert)
+import Data.Binary (decode)
+import Data.ByteString.Lazy (fromStrict)
+#endif
 import Network.CryptoNote.Crypto.Hash.Types (RawHash)
 import Data.ByteString
 
+
+#if !defined(USE_FFI_CRYPTO)
+hash :: ByteString -> RawHash
+hash = decode . fromStrict . convert . hashWith Keccak_256
+#else
 hash :: ByteString -> RawHash
 hash = FFI.cnFastHash
+#endif
