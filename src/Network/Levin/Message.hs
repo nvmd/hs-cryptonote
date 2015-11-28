4d01bbb222d821a91aaab97729df940b67bde2e1
Sergey Kazenyuk <kazenyuk@gmail.com>
2015-11-28 21:55:39 +0300

    P2P debug commands flag refactoring
    * Always compile p2p debug commands'
    * Flag should affect handler compilation only (and we don't have any yet)
    * Better flag name

--- src/Network/Levin/Message.hs
+++ src/Network/Levin/Message.hs
@@ -2,28 +2,22 @@
 
 module Network.Levin.Message where
 
-import Data.Binary (Binary, get, put)
-import Data.Binary.Get (getWord32le)
-import Data.Binary.Put (putWord32le)
+import Data.Binary (Binary)
 
 import Network.Levin.Command.Handshake (CommandHandshake)
 import Network.Levin.Command.Ping (CommandPing)
 import Network.Levin.Command.TimedSync (CommandTimedSync)
-#if ENABLE_DEBUG_COMMANDS
 import Network.Levin.Command.Debug
-#endif
 
 
 -- net_node.h
 -- p2p_protocol_defs.h
 
-
-data Message = MHandshake       (CommandHandshake ())
-             | MTimedSync       (CommandTimedSync ())
-             | MPing            CommandPing
-#if ENABLE_DEBUG_COMMANDS
-             | MReqStatInfo     (CommandRequestStatInfo ())      -- ^ Debug command.
-             | MReqNetworkState CommandRequestNetworkState  -- ^ Debug command.
-             | MReqPeerId       CommandRequestPeerId        -- ^ Debug command.
-#endif
-             deriving (Show, Eq)
+data (Binary hp, Binary tp, Binary si) =>
+     Message hp tp si = MHandshake       (CommandHandshake hp)
+                      | MTimedSync       (CommandTimedSync tp)
+                      | MPing            CommandPing
+                      | MReqStatInfo     (CommandRequestStatInfo si) -- ^ Debug command.
+                      | MReqNetworkState CommandRequestNetworkState  -- ^ Debug command.
+                      | MReqPeerId       CommandRequestPeerId        -- ^ Debug command.
+                      deriving (Show, Eq)
